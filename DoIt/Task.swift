3e79ef39a1dcd742ae36a7d388b997915e9d5424
Tyson Rinderknecht2 <nosyt@me.com>
2016-12-19 18:05:49 -0600

    Begin lecture #59

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Tyson Rinderknecht2 on 12/19/16.
+//  Copyright © 2016 Tyson Rinderknecht. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
