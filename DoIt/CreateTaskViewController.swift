a50a7b71d1120807b9dfcf50be6eb64abea97c7e
Tyson Rinderknecht2 <nosyt@me.com>
2016-12-19 19:01:56 -0600

    Begin lecture 61 builds and runs successfully

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Tyson Rinderknecht2 on 12/19/16.
+//  Copyright © 2016 Tyson Rinderknecht. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+}
